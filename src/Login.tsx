35f0315b9a7766d7a37d90cc3aaab292e75ba4cf
Duc-Thomas <duckiduc.dev@gmail.com>
2022-06-29 00:53:59 +0200

    Login page

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,12 +1,24 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+} from "react-native";
+import Input from "../src/components/Input";
+import PinkButton from "../src/components/PinkButton";
 
 export default function Home() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-
-      <Text style={styles.text}>Open up Home.tsx to start working on your Home!</Text>
+      <View style={styles.header}>
+        <Image style={styles.logo} source={require("../assets/frame.svg")} />
+        <Text style={styles.text}>Se connecter à votre compte</Text>
+      </View>
+      <Input label="Adresse email" placeholder="john.doe@email.com" />
+      <Input label="Mot de passe" placeholder="Entrer votre mot de passe" />
+      <PinkButton label="Se connecter"/>
     </View>
   );
 }
@@ -14,12 +26,27 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#0d0d0d",
+    backgroundColor: "#0D0D0D",
+    alignItems: "center",
+    padding: 25,
+  },
+
+  header: {
     alignItems: "center",
     justifyContent: "center",
+    marginVertical: 50,
   },
 
   text: {
     color: "#ffffff",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+
+  logo: {
+    width: "70%",
+    height: undefined,
+    aspectRatio: 1 / 1,
+    marginBottom: 50,
   },
 });
---
+++ src/components/PinkButton.tsx
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, Pressable } from "react-native";
+
+export default function PinkButton(props) {
+  return (
+    <Pressable style={styles.submit}>
+      <Text style={styles.text}>{props.label}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  submit: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 25,
+    elevation: 3,
+    backgroundColor: "#DA90DA",
+    position: "absolute",
+    bottom: 50,
+  },
+
+  text: {
+    color: "#ffffff",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+});
